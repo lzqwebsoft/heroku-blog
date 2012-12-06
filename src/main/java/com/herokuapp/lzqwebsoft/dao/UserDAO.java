9618758f9932cabbe9ab6af55c023575fb3a76fa
johnny <lzqwebsoft@gmail.com>
2012-12-06 22:51:53 +0800

    添加登录示例

--- src/main/java/com/herokuapp/lzqwebsoft/dao/UserDAO.java
+++ src/main/java/com/herokuapp/lzqwebsoft/dao/UserDAO.java
@@ -4,41 +4,60 @@ import com.herokuapp.lzqwebsoft.pojo.User;
 import java.util.List;
 import java.util.ArrayList;
 
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Repository;
 
-public class UserDAO extends HibernateDaoSupport {
+@Repository("userDAO")
+public class UserDAO{
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    private HibernateTemplate ht = null;
+    private HibernateTemplate getHibernateTemple() {
+        if(ht==null) {
+            ht = new HibernateTemplate(sessionFactory);
+        }
+        return ht;
+    }
 
     public void save(User user) {
-        getHibernateTemplate().save(user);
+        getHibernateTemple().save(user);
         
         System.out.println("A user saved!");
     }
 
     public void update(User user) {
-    	getHibernateTemplate().update(user);
+        getHibernateTemple().update(user);
 
         System.out.println("A user updated!");
     }
 
 	public User getUser(Long userid) {
-		User user = (User)getHibernateTemplate().get(User.class, userid);
+		User user = (User)getHibernateTemple().get(User.class, userid);
 		return user;
 	}
 
 	public void delete(User user) {
-		getHibernateTemplate().delete(user);
+	    getHibernateTemple().delete(user);
 	}
 	
-	public User getUser(final String username, final String password) {
+	@SuppressWarnings("unchecked")
+    public User getUser(String username, String password) {
 		System.out.println("query user by name and password.");
-//		getHibernateTemplate()
+		List<User> list = (List<User>)getHibernateTemple().find("from User u where u.userName=? and u.password=?", new Object[]{username, password});
+		if(list!=null&&list.size()>0) {
+		    User user = list.get(0);
+		    return user;
+		}
 		return null;
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<User> getUsers() {
 		List<User> users = new ArrayList<User>();
-		users = (List<User>)getHibernateTemplate().find("from users");
+		users = (List<User>)sessionFactory.getCurrentSession().createCriteria("from user");
 
         System.out.println("Get all users!");
 
