9870d63c0ecd5be9f7d8bf0527633e68c93ccb4e
lzqwebsoft <751939573@qq.com>
2013-01-09 20:38:52 +0800

    新增评论时发送邮件

--- src/main/java/com/herokuapp/lzqwebsoft/controller/CommentController.java
+++ src/main/java/com/herokuapp/lzqwebsoft/controller/CommentController.java
@@ -1,25 +1,40 @@
 package com.herokuapp.lzqwebsoft.controller;
 
 import java.util.List;
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.herokuapp.lzqwebsoft.pojo.Article;
 import com.herokuapp.lzqwebsoft.pojo.Comment;
 import com.herokuapp.lzqwebsoft.pojo.User;
+import com.herokuapp.lzqwebsoft.service.ArticleService;
 import com.herokuapp.lzqwebsoft.service.CommentService;
+import com.herokuapp.lzqwebsoft.service.UserService;
 import com.herokuapp.lzqwebsoft.util.CommonConstant;
+import com.herokuapp.lzqwebsoft.util.MailUtil;
 
 @Controller
 public class CommentController {
 	@Autowired
 	private CommentService commentService;
+	
+	@Autowired
+	private ArticleService articleService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@RequestMapping("/comment/add")
 	public String add(Comment comment, String parent_comment_id,
@@ -31,11 +46,26 @@ public class CommentController {
 			parnetComment.setId(parentId);
 			comment.setParentComment(parnetComment);
 		}
+		
+		String articleId = comment.getArticle().getId();
 		// 判断用户是否登录,则博主不用输入昵称与网址
 		User user = (User)session.getAttribute(CommonConstant.LOGIN_USER);
 		if(user!=null) {
 			comment.setReviewer(user.getUserName());
 			comment.setWebsite(request.getRemoteHost()+":"+request.getLocalPort()+request.getContextPath());
+		} else {
+			// 发送邮件给博言主，通知有新评论
+			user = userService.getBlogOwner();
+			if(user.getEmail()!=null){
+				Locale locale = request.getLocale();
+				Article article = articleService.get(comment.getArticle().getId());
+				String link = new StringBuffer("http://").append(request.getRemoteHost()).append(request.getContextPath())
+				      .append("/show/").append(articleId).append(".html").toString();
+				String content = messageSource.getMessage("email.addComment.content", new Object[]{user.getUserName(), 
+						article.getTitle(), link}, locale);
+				String title = messageSource.getMessage("email.addComment.title", null, locale);
+				MailUtil.sendEMail(user.getEmail(), title, content);
+			}
 		}
 		
 		// 为网址加上http
@@ -44,7 +74,6 @@ public class CommentController {
 			comment.setWebsite("http://"+webSite);
 		commentService.save(comment);
 		
-		String articleId = comment.getArticle().getId();
 		List<Comment> comments = commentService.getAllParentComment(articleId);
 		model.addAttribute("comments", comments);
 		
