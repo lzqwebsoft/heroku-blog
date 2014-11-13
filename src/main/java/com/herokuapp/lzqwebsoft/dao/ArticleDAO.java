e1b4046412b8cb0ff7c67175c08527e303da81fc
Johnny Luo <lzqwebsoft@gmail.com>
2014-11-13 17:33:02 +0800

    上一篇，下一篇过滤草稿文章

--- src/main/java/com/herokuapp/lzqwebsoft/dao/ArticleDAO.java
+++ src/main/java/com/herokuapp/lzqwebsoft/dao/ArticleDAO.java
@@ -109,7 +109,7 @@ public class ArticleDAO extends PageBaseDAO<Article> {
 	
 	@SuppressWarnings("unchecked")
 	public Article getPreviousArticle(final Article article) {
-		final String queryString = "from Article as art where art.createAt<? order by art.createAt desc";
+		final String queryString = "from Article as art where art.createAt<? and art.status=1 order by art.createAt desc";
 		List<Article> list =  (List<Article>) getHibernateTemple().executeFind(
 				new HibernateCallback() {
 					public List<Article> doInHibernate(Session session)
@@ -129,7 +129,7 @@ public class ArticleDAO extends PageBaseDAO<Article> {
 	
 	@SuppressWarnings("unchecked")
 	public Article getNextArticle(final Article article) {
-		final String queryString = "from Article as art where art.createAt>? order by art.createAt asc";
+		final String queryString = "from Article as art where art.createAt>? and art.status=1 order by art.createAt asc";
 		List<Article> list =  (List<Article>) getHibernateTemple().executeFind(
 				new HibernateCallback() {
 					public List<Article> doInHibernate(Session session)
