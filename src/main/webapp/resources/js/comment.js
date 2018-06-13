f17dea6f3f40cbfcef0ed1b4a189b6d9e66a8fda
Johnny Luo <lzqwebsoft@gmail.com>
2018-06-13 20:40:46 +0800

    修正移动端判断

--- src/main/webapp/resources/js/comment.js
+++ src/main/webapp/resources/js/comment.js
@@ -8,8 +8,8 @@ function browserVersions(){
 		gecko : u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, // 火狐内核
 		mobile : !!u.match(/AppleWebKit.*Mobile.*/), // 是否为移动终端 
 		ios : !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), // ios终端
-		android : u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, // android终端或者uc浏览器
-		iPhone : u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, // 是否为iPhone或者QQHD浏览器
+		android : u.indexOf('Android') > -1, // android终端或者uc浏览器
+		iPhone : u.indexOf('iPhone') > -1, // 是否为iPhone或者QQHD浏览器
 		iPad: u.indexOf('iPad') > -1, // 是否iPad
 		webApp : u.indexOf('Safari') == -1,// 是否web应该程序，没有头部与底部
 		google:u.indexOf('Chrome')>-1
