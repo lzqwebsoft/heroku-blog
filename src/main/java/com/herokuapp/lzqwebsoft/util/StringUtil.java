94575fc90a83e64eb31213e62e0ab00ae181adeb
Johnny <lzqwebsoft@gmail.com>
2018-08-01 18:53:32 +0800

    字符过滤

---
+++ src/main/java/com/herokuapp/lzqwebsoft/util/StringUtil.java
@@ -0,0 +1,56 @@
+package com.herokuapp.lzqwebsoft.util;
+
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
+public class StringUtil {
+
+    public static String xssCharEncode(String html, String contextPath) {
+        String value = html;
+        String cleanValue = null;
+        if (value != null) {
+            cleanValue = Normalizer.normalize(value, Normalizer.Form.NFD);
+
+            // Avoid null characters
+            cleanValue = cleanValue.replaceAll("\0", "");
+
+            // Avoid anything between script tags
+            Pattern scriptPattern = Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid anything in a src='...' type of expression
+//            String emojiPattern = "(?!(.*?)" + contextPath + "\\/resources\\/js\\/ke4\\/plugins\\/emoticons\\/images\\/\b{1,2}\\.gif)";
+            scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*['\"](.*?)['\"]", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Remove any lonesome </script> tag
+            scriptPattern = Pattern.compile("</script>", Pattern.CASE_INSENSITIVE);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Remove any lonesome <script ...> tag
+            scriptPattern = Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid eval(...) expressions
+            scriptPattern = Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid expression(...) expressions
+            scriptPattern = Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid javascript:... expressions
+            scriptPattern = Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid vbscript:... expressions
+            scriptPattern = Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid onload= expressions
+            scriptPattern = Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+        }
+        return cleanValue;
+    }
+}
