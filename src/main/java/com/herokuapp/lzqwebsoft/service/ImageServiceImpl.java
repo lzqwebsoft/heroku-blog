63e8bb620cad5ea4b2c8227e06e5f6faf0b43a3d
Johnny <lzqwebsoft@gmail.com>
2018-08-06 16:48:43 +0800

    自定义400

--- src/main/java/com/herokuapp/lzqwebsoft/service/ImageServiceImpl.java
+++ src/main/java/com/herokuapp/lzqwebsoft/service/ImageServiceImpl.java
@@ -11,12 +11,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.herokuapp.lzqwebsoft.dao.ImageDAO;
 import com.herokuapp.lzqwebsoft.pojo.Image;
+import com.herokuapp.lzqwebsoft.pojo.Page;
 import com.herokuapp.lzqwebsoft.util.QiniuUtil;
 import com.herokuapp.lzqwebsoft.util.SHA1Util;
 
 @Service("imageService")
 public class ImageServiceImpl implements ImageService {
-    
+
     @Autowired
     private ImageDAO imageDAO;
 
@@ -25,8 +26,7 @@ public class ImageServiceImpl implements ImageService {
         String originalFilename = file.getOriginalFilename();
         // 使用SHA1Util产生一个随机的16位与时间截组成上传图片id
         SimpleDateFormat format = new SimpleDateFormat("yyyyMMddhhmmss");
-        StringBuffer id = new StringBuffer(format.format(new Date()))
-                .append(SHA1Util.generateSalt());
+        StringBuffer id = new StringBuffer(format.format(new Date())).append(SHA1Util.generateSalt());
         image.setId(id.toString());
         image.setFileName(originalFilename);
         image.setCreateAt(new Date());
@@ -36,11 +36,11 @@ public class ImageServiceImpl implements ImageService {
             // 添加七牛云文件备份
             image = QiniuUtil.upload(image);
             imageDAO.save(image);
-            return "images/show/"+ image.getId() +".html";
+            return "images/show/" + image.getId() + ".html";
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
         return "resources/images/NoImageShow.jpg";
     }
 
@@ -55,9 +55,14 @@ public class ImageServiceImpl implements ImageService {
     }
 
     @Override
+    public Page<Image> getAllImagesByPage(int pageNo, int pageSize) {
+        return imageDAO.getAllImagesByPage(pageNo, pageSize);
+    }
+
+    @Override
     public void delete(String id) {
         Image image = imageDAO.getImageById(id);
-        if(image!=null) {
+        if (image != null) {
             // 添加七牛云删除文件
             QiniuUtil.delete(image);
             imageDAO.delete(image);
