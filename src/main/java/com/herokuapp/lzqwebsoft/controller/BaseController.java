91a0d6b7ed1b50f755ed2a68244e0c7ef83f7080
Johnny <lzqwebsoft@gmail.com>
2018-08-29 16:40:17 +0800

    IP归属地修正

--- src/main/java/com/herokuapp/lzqwebsoft/controller/BaseController.java
+++ src/main/java/com/herokuapp/lzqwebsoft/controller/BaseController.java
@@ -98,8 +98,7 @@ public class BaseController {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            return "";
         }
+        return "";
     }
 }
